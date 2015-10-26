08b5a5bbdd879984d8f31636a3beb323b49e928e
Jade Allen Cook <jadeallencook@gmail.com>
2015-10-25 20:43:00 -0600

    navigation and page caller

---
+++ include-links.php
@@ -0,0 +1,22 @@
+<?php
+
+    // link text & GET url
+    $navLinks = array(
+        array('home', 'home'),
+        array('info', 'info'),
+        array('contact', 'contact')
+    );
+    
+    // display each link
+	for ( $i = 0; $i <= count($navLinks) - 1; $i++ ) {
+		echo '<a href="?page=' . $navLinks[$i][1] . '">' . $navLinks[$i][0] . '</a>';
+	}
+	
+	// include the page called 
+	if ( isset($_GET['page']) && file_exists('pages/' . $_GET['page'] . '.php') ) {
+    	include_once('pages/' . $_GET['page'] . '.php');
+	} else {
+    	include_once('pages/home.php');
+	}
+
+?>
