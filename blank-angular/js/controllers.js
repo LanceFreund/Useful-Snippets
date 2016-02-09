18bcef50befa2a999cd57550147626b9390fce81
Jade Allen Cook <jadeallencook@gmail.com>
2016-02-09 13:52:01 -0700

    Added Foundation 6

--- blank-angular/js/controllers.js
+++ blank-angular/js/controllers.js
@@ -4,6 +4,16 @@
 app.controller('data', function ($scope) {
     if (business.titleTag == true) $('title').text(business.name + ' | ' + business.tag);
     else $('title').text(business.name);
+    // apply primary font to body
+    $('head').append('<link href="' + fonts.primary.link + '" type="text/css" rel="stylesheet" />');
+    $('body').css({
+        fontFamily: fonts.primary.name + ', ' + fonts.primary.secondary
+    });
+    // apply header font to all headers
+    $('head').append('<link href="' + fonts.header.link + '" type="text/css" rel="stylesheet" />');
+    $('h1, h2, h3, h4, h5, h6').css({
+        fontFamily: fonts.header.name + ', ' + fonts.header.secondary
+    });
 });
 
 // home.html
